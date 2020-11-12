719ad027bf2b3e1c31af9c20e06749ea25c82a85
yoonemong <yoonemong@naver.com>
2020-11-12 21:22:02 +0900

    Test

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,4 +1,47 @@
 package hello.core.lifecycle;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+    }
+
+    @Test
+    public void lifeCycleTest1(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClientTest acBean = ac.getBean(NetworkClientTest.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+        @Bean
+        public NetworkClientTest networkClientTest(){
+            NetworkClientTest networkClientTest = new NetworkClientTest();
+            networkClientTest.setUrl("http://naver.com");
+            return networkClientTest;
+        }
+
+
+    }
+
 }
--- src/test/java/hello/core/lifecycle/NetworkClientTest.java
+++ src/test/java/hello/core/lifecycle/NetworkClientTest.java
@@ -1,4 +1,41 @@
 package hello.core.lifecycle;
 
-public class NetworkClientTest {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClientTest implements InitializingBean, DisposableBean {
+    
+    private String url;
+
+    public NetworkClientTest() {
+        System.out.println("클라이언트 생성 : " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public void disconnect(){
+        System.out.println("close : " +url);
+    }
+    
+    public void connect(){
+        System.out.println("connect : " + url);
+    }
+    
+    public void call(){
+        System.out.println("url : " + url + "connect : " + "호출");
+    }
+    
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call();
+    }
 }
